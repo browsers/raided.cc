905218c5137813a1c0e8f41c95a5f17d4e1c9485
vxqj <tiktokalt1276@proton.me>
2026-09-26 16:28:08 +1000

    g

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,9 @@ export default function Home() {
               exit={{ opacity: 0, y: -8 }}
               transition={{ duration: 0.45, ease: [0.23, 1, 0.32, 1] }}
             >
-              <BrandTitle fontSize={26} />
+              <div style={{ marginTop: -20 }}>
+                <BrandTitle fontSize={26} />
+              </div>
               <div style={{ marginTop: 40 }}>
                 <ClaimHandleForm onComplete={handleClaimed} />
               </div>
